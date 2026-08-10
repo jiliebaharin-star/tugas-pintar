9362424f021abe1d7cb7ec910bf6d62c699fdd7d
jiliebaharin-star <jiliebaharin@gmail.com>
2026-08-10 11:05:06 +0800

    Add Next.js App Router layout

---
+++ app/layout.js
@@ -0,0 +1,4 @@
+import './globals.css';
+
+export const metadata={title:'Tugas Pintar',description:'Pengurusan tugasan berasaskan emel'};
+export default function RootLayout({children}){return <html lang="ms"><body>{children}</body></html>}
